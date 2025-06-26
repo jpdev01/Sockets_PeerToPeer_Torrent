a1be3f430b3be0003299399917e55a71c94ca9d0
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2025-06-26 15:12:48 -0300

    upd: porta padrao é a 5001

--- src/main/java/peer/Main.java
+++ src/main/java/peer/Main.java
@@ -8,6 +8,10 @@ public class Main {
         // args[0] = nome do peer (ex: "127.0.0.1:5001")
         // args[1...] = pasta de pedaços
 
+        String port = args[0];
+        if (port == null || port.isEmpty()) {
+            port = "5001";
+        }
         String peerId = Inet4Address.getLocalHost().getHostAddress().concat(":").concat(args[0]);
 
         String folderName = args[1];
